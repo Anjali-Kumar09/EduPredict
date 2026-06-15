7f88c0bd2e3e26e1e67ca329773bce5d624cb7d6
Anjali Kumari <anjalikumari16434@gmail.com>
2026-06-15 21:39:37 +0530

    Comment out heavy ML packages for Render build

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
-# Install dependencies
+pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
-
-# Run database migrations
 python manage.py migrate
-
-# Collect static files
 python manage.py collectstatic --noinput
